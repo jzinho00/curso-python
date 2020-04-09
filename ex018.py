77da85bdf5472186c162eb0c8f66030e47f1b01c
João Meirinho <joao_meirinho@outlook.com>
2020-04-09 19:24:23 -0300

    add ex016 até ex018

---
+++ ex018.py
@@ -0,0 +1,6 @@
+import math
+a = float(input('Digite o ângulo que você deseja: '))
+print('O ângulo de {} tem o seno de {:.2f}'.format(a, math.sin(a)))
+print('O ângulo de {} tem o cosseno de {:.2f}'.format(a, math.cos(a)))
+print('O ângulo de {} tem a tangente de {:.2f}'.format(a, math.tan(a)))
+
